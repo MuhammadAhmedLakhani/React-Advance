478501f7c9cc6a05d01aebf8866359411e7a369e
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-24 16:38:53 +0500

    Hooks:Todo (updation)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ import { useState } from 'react';
 function App() {
 
   
-let [todos, setTodos] = useState([{value:"Ahad"},{value:"Ahmed"}])
+let [todos, setTodos] = useState([
+  {
+    value:"Ahad",
+    disabled:true
+},
+  {
+  value:"Ahmed",
+  disabled:true
+}])
 
 let [value,setValue] = useState("")
 
@@ -22,14 +30,51 @@ const deleteToDo = (index) => {
   setTodos(oldtodos)
 }
 
+const editToDo = (value) => {
 
-console.log(todos,"todos")
+    console.log(value)
+
+    value.disabled = false
+
+    console.log(value,"updated")
+    console.log(todos,"todos")
+    setTodos([...todos])
+
+}
+
+const updateTodo = (val) => {
+  // v.value = event.target.value
+  // setTodos([todos])
+
+  console.log(val)
+  val.disabled = true;
+  console.log(val,"updateds")
+  setTodos([...todos])
+
+}
+
+
+
+
   return (
     <div>
       <input type="text"placeholder='Enter todos' value={value}    onChange={(e)=> setValue(e.target.value)} />
       <button onClick={addToDo}>Add ToDo</button>
+
+
+      <br/>
       <ul>
-          {todos.map((v,i)=><li key={i}>{v.value}
+          {todos.map((v,i)=><li key={i}>
+
+            <input  disabled = {v.disabled}  type="text" defaultValue={v.value} onChange={(e)=> v.value = e.target.value}  />
+            
+
+            {v.disabled?            //TRENERY OPERATOR
+              
+              <button onClick={()=> editToDo(v)}>Edit</button>
+              :
+              <button onClick={()=>updateTodo(v)}>Update </button>
+            }
             <button onClick={()=>deleteToDo(i)}>Delete</button>
           </li>
           )}
